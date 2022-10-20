443a34fd91c75c1a5c47f367b45d5d28e5b7a998
V1ct-0R <913606086@qq.com>
2022-10-20 17:34:52 +0800

    About页面增加源码链接

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,5 +3,8 @@
     <div style="margin-top:10px;">本系统的功能为城市部分信息的查询</div>
     <div style="margin-top:10px;">在输入框中输入城市名后搜索即可得到城市的基本概况、环境状况、天气预报、社会新闻等信息</div>
     <div style="margin-top:10px;">目前本系统仅支持国内城市信息的查询，请人为保证输入的正确性</div>
+    <div style="margin-top:10px;">
+      <a :href="'https://github.com/V1ct-0R/SOA-individual-project'">查看项目源码</a>
+    </div>
   </div>
 </template>
