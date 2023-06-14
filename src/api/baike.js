b59f3bdd258b2ca2ff539e339cdfa261192f1050
V1ct-0R <913606086@qq.com>
2023-06-14 20:32:08 +0800

    更新接口

--- src/api/baike.js
+++ src/api/baike.js
@@ -1,23 +1,23 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 //根据城市名给出百度百科的简介
 export function getInfoFromBaidu(cityname) {
-    return request({
-        url: 'baidubaike/api/openapi/BaikeLemmaCardApi',
-        method: 'get',
-        params: {
-            appid: 379020,
-            bk_key: cityname
-        }
-    })
+  return request({
+    url: "baidubaike/api/openapi/BaikeLemmaCardApi",
+    method: "get",
+    params: {
+      appid: 379020,
+      bk_key: cityname,
+    },
+  });
 }
 //根据城市名给出维基百科的简介
 export function getInfoFromWiki(cityname) {
-    return request({
-        url: 'maitanbang/api/wkbaike/index',
-        method: 'get',
-        params: {
-            key: 'RurN62F7k9gY5c2f3XHIHoMCQJ',
-            keyword: cityname
-        }
-    })
+  return request({
+    url: "maitanbang/apis/mtbwkbaike/",
+    method: "get",
+    params: {
+      key: "RurN62F7k9gY5c2f3XHIHoMCQJ",
+      keyword: cityname,
+    },
+  });
 }
